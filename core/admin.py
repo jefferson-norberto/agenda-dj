c0ca029dfabb5189f5d04e29901178ffd3307e6f
jefferson-norberto <jefferson.norbertto@gmail.com>
2022-08-09 20:23:02 -0300

    Table events created

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.models import Event
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'event_date', 'create_date')
+    list_filter = (['user'])
 
 # Register your models here.
+admin.site.register(Event, EventAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
+from tabnanny import verbose
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=180)
+    description = models.TextField(blank=True, null=True)
+    event_date = models.DateTimeField()
+    create_date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'event'
+        verbose_name = 'Event'
+        verbose_name_plural = 'Events'
+    
+    def __str__(self) -> str:
+        return self.title
